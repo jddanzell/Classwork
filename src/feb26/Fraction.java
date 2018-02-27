a1e15d92475484682d2b8a0f7446415abfd7cf9c
jddanzell <Josh4102000@gmail.com>
2018-02-27 08:59:38 -0600

    Added Classwork folder to keep track of coding in class. Added Fraction class.

---
+++ src/feb26/Fraction.java
@@ -0,0 +1,25 @@
+package feb26;
+
+public class Fraction implements Comparable<Fraction>
+{
+    private int num;
+    private int denom;
+
+   public Fraction(int num, int denom)
+   {
+       this.num = num;
+       this.denom = denom;
+       simplify();
+   }
+
+    @Override
+    public int compareTo(Fraction o) {
+        return 0;
+    }
+
+    private void simplify()
+    {
+
+    }
+}
+
